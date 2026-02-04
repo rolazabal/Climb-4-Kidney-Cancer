3289fe9087b0bc67c69470cbde373658cce7dec1
Ricardo Olazabal <ricardoolazabaljr@gmail.com>
2026-02-03 20:27:44 -0500

    skeleton for mountains API
    get method just returns the hello world json object

--- Services/Mountains/mountains.py
+++ Services/Mountains/mountains.py
@@ -1,1 +1,10 @@
-# hello world
+#type "fastapi dev mountains.py" in console to run
+
+from fastapi import FastAPI, Request, Response, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello world."}
