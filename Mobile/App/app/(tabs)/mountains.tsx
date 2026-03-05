b3c8a7f5c94393200ea7eb410e32fc1cfed332fd
Chan Kang <122942792+Chan-Kang415@users.noreply.github.com>
2026-03-05 18:42:21 -0500

    Minor Fixes
    1. Backend function error in mountains.py --> deleting didn't work
    
    2. Url Change
    
    3. .App may cause bug, so I changed to small a

--- Mobile/App/app/(tabs)/mountains.tsx
+++ Mobile/App/app/(tabs)/mountains.tsx
@@ -14,7 +14,7 @@ const theme = {
 
 function Mountains() {
 
-    const mountains_url = "http://127.0.0.1:8000";
+    const mountains_url = "http://10.0.2.2:8000";
 
     const mountainsClimbed = new Array();
 
