1c38abb4a727a246dc9bd4a39f27402c7b03672c
sangeldiaz-afk <sangeldiaz3696@gmail.com>
2026-03-19 17:53:06 -0400

    Groups Service Docker Integration

--- Services/Gateway/gateway.py
+++ Services/Gateway/gateway.py
@@ -6,7 +6,7 @@ app = FastAPI()
 USERS_SERVICE = "http://users-service:8000"
 MOUNTAINS_SERVICE = "http://mountains-service:8000"
 PROGRESS_SERVICE = "http://progress-service:8000"
-#GROUP_SERVICE = "http://group-service:8000"
+GROUP_SERVICE = "http://groups-service:8000"
 EVENTS_SERVICE = "http://event-service:8000"
 
 async def forward_request(request: Request, service_url: str, path: str):
@@ -36,6 +36,12 @@ async def mountains_proxy(path: str, request: Request):
 @app.api_route("/progress/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def progress_proxy(path: str, request: Request):
     return await forward_request(request, PROGRESS_SERVICE, path)
+
+
+@app.api_route("/progress/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
+async def progress_proxy(path: str, request: Request):
+    return await forward_request(request, GROUP_SERVICE, path)
+
 
 @app.api_route("/progress/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def progress_proxy(path: str, request: Request):
